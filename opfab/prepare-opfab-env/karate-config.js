69f9a5d95b7f2fd30f2c5061ffcc6ae2d76ac566
OUESLATI Wyem Ext <oueslatiwye@gm0winl269.bureau.si.interne>
2021-04-13 16:42:58 +0200

    [LC-363] Scan code with Fossology

--- opfab/prepare-opfab-env/karate-config.js
+++ opfab/prepare-opfab-env/karate-config.js
@@ -1,4 +1,13 @@
 function fn() {
+
+// Copyright (c) 2020-2021, RTE (https://www.rte-france.com)
+// Copyright (c) 2020-2021 RTE international (https://www.rte-international.com)
+// See AUTHORS.txt
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+// This file is part of the Let’s Coordinate project.
     
     var config = { // base config JSON
       opfabUrl: 'http://localhost/',
