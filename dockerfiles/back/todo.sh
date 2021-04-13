17d8ced4c17f325a38d0961252bad1234627fff4
OUESLATI Wyem Ext <oueslatiwye@gm0winl269.bureau.si.interne>
2021-04-13 16:42:58 +0200

    [LC-363] Scan code with Fossology
    Signed-off-by: OUESLATI Wyem Ext <wyem.oueslati@rte-france.com>

--- dockerfiles/back/todo.sh
+++ dockerfiles/back/todo.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Copyright (c) 2020-2021, RTE (https://www.rte-france.com)
+# Copyright (c) 2020-2021 RTE international (https://www.rte-international.com)
+# See AUTHORS.txt
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+# This file is part of the Let’s Coordinate project.
+
 jarVersion=$(grep -E '<version>.*</version>' ../../letsco-api/pom.xml | head -1 | sed 's/.*<version>\(.*\)<\/version>/\1/')
 if [ -z $1 ]; then
   tag=$jarVersion
--- letsco-api/src/main/java/org/lfenergy/letscoordinate/backend/enums/BasicGenericNounEnum.java
+++ letsco-api/src/main/java/org/lfenergy/letscoordinate/backend/enums/BasicGenericNounEnum.java
@@ -1,11 +1,20 @@
+/*
+ * Copyright (c) 2020-2021, RTE (https://www.rte-france.com)
+ * Copyright (c) 2020-2021 RTE international (https://www.rte-international.com)
+ * See AUTHORS.txt
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of the Let’s Coordinate project.
+ */
+
 package org.lfenergy.letscoordinate.backend.enums;
 
 import lombok.Getter;
 import org.lfenergy.operatorfabric.cards.model.SeverityEnum;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 import static org.lfenergy.operatorfabric.cards.model.SeverityEnum.*;
 
