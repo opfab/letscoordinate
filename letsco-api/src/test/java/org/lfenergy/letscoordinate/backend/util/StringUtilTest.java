a4c657040b4617294b44e4fb1664e0c572c7c5b8
OUESLATI Wyem Ext <wyem.oueslati@rte-france.com>
2021-06-17 17:49:10 +0200

    [LC-567] Save original input file and generate unique file identifier
    Signed-off-by: OUESLATI Wyem Ext <wyem.oueslati@rte-france.com>

--- letsco-api/src/test/java/org/lfenergy/letscoordinate/backend/util/StringUtilTest.java
+++ letsco-api/src/test/java/org/lfenergy/letscoordinate/backend/util/StringUtilTest.java
@@ -13,9 +13,13 @@ package org.lfenergy.letscoordinate.backend.util;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.lfenergy.letscoordinate.backend.dto.eventmessage.EventMessageDto;
+import org.lfenergy.letscoordinate.backend.dto.eventmessage.header.BusinessDataIdentifierDto;
+import org.lfenergy.letscoordinate.backend.dto.eventmessage.header.HeaderDto;
 import org.lfenergy.letscoordinate.backend.dto.eventmessage.payload.TextDataDto;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -100,6 +104,45 @@ public class StringUtilTest {
     public void getFilenameWithoutExtension_notContainingExtension() {
         String input = "file1234_2021";
         assertEquals("file1234_2021", StringUtil.getFilenameWithoutExtension(input));
+    }
+
+    @Test
+    public void generateUniqueFileIdentifier_eventMessageDto_notNull() {
+        EventMessageDto eventMessageDto1 = initEventMessageDto();
+        EventMessageDto eventMessageDto2 = initEventMessageDto();
+        assertAll(
+                () -> assertFalse(eventMessageDto1 == eventMessageDto2),
+                () -> assertEquals(StringUtil.generateUniqueFileIdentifier(eventMessageDto1),
+                        StringUtil.generateUniqueFileIdentifier(eventMessageDto2))
+        );
+    }
+
+    private EventMessageDto initEventMessageDto() {
+        EventMessageDto eventMessageDto = new EventMessageDto();
+
+        HeaderDto header = eventMessageDto.getHeader();
+        header.setVerb("create");
+        header.setNoun("noun");
+        header.setTimestamp(Instant.parse("2021-05-31T05:13:00Z"));
+        header.setSource("source");
+        header.setMessageId("messageId");
+        header.getProperties().setFormat("JSON");
+        BusinessDataIdentifierDto bdi = header.getProperties().getBusinessDataIdentifier();
+        bdi.setMessageType("messageType");
+        bdi.setMessageTypeName("messageTypeName");
+        bdi.setBusinessDayFrom(Instant.parse("2021-05-31T00:00:00Z"));
+        bdi.setBusinessDayTo(Instant.parse("2021-05-31T23:59:59Z"));
+        bdi.setProcessStep("processStep");
+        bdi.setTimeframe("timeframe");
+        bdi.setBusinessApplication("businessApplication");
+        bdi.setTimeframeNumber(1);
+        bdi.setSendingUser("sendingUser");
+        bdi.setCaseId(null);
+        bdi.setFileName(null);
+        bdi.setTso(null);
+        bdi.setBiddingZone(null);
+
+        return eventMessageDto;
     }
 
 }
