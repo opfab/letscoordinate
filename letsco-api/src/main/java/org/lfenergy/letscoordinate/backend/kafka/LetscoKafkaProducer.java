e1bf487eab70daceae2e4c8fa78b237a16977a89
OUESLATI Wyem Ext <wyem.oueslati@rte-france.com>
2021-07-27 19:50:40 +0200

    [LC-569] Create APIs to manage specific messages output files by third applications
    Signed-off-by: OUESLATI Wyem Ext <wyem.oueslati@rte-france.com>

--- letsco-api/src/main/java/org/lfenergy/letscoordinate/backend/kafka/LetscoKafkaProducer.java
+++ letsco-api/src/main/java/org/lfenergy/letscoordinate/backend/kafka/LetscoKafkaProducer.java
@@ -20,7 +20,6 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.lfenergy.letscoordinate.backend.dto.KafkaFileWrapperDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
